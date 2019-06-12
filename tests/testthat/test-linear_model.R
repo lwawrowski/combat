a6030e8712ff69227eec84d9dd7f2a6fffa4a926
lwawrowski <lukasz8989@gmail.com>
2019-06-12 18:22:21 +0200

    adding tests to linear_model function

--- tests/testthat/test-linear_model.R
+++ tests/testthat/test-linear_model.R
@@ -1,11 +1,14 @@
 context("Linear model")
 
 test_that("check results", {
-  expect_equal(linear_model(c(1,2), c(10,2)), c(2,0))
+  expect_equal(linear_model(a = c(1,2), b = c(10,2)), c(2,0))
+  expect_length(linear_model(a = c(1,5), b = c(10,1)), 2)
 })
 
 test_that("check errors", {
-  expect_error(linear_model(c(1,2), c(10,NA)))
-  expect_error(linear_model(c(2,10), c(2,20)))
+  expect_error(linear_model(a = c(1,2), b = c(10,NA)))
+  expect_error(linear_model(a = c(2,10), b = c(2,20)))
+  expect_error(linear_model(a = c(2,10,5), b = c(3,20)))
+  expect_error(linear_model(a = c("2","10"), b = c(5,20)))
 })
 
