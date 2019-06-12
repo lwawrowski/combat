fa1f638e4f865bb398de26af4c8738400f82df26
lwawrowski <lukasz8989@gmail.com>
2019-06-12 23:34:56 +0200

    changing tests due to modification in set.seed in R 3.6.0

--- tests/testthat/test-hexs_mobility.R
+++ tests/testthat/test-hexs_mobility.R
@@ -7,15 +7,15 @@ p2 <- draw_units(price_limit = 50, units_data = units)
 units_game <- draw_hexs(p1, p2)
 
 test_that("check results", {
-  expect_length(hexs_mobility(hex_id = "ID9", units_data = units_game, dist_matrix_data = dist_matrix), 127)
-  expect_length(hexs_mobility(hex_id = "ID352",mobility = 2, units_data = units_game, dist_matrix_data = dist_matrix), 9)
+  expect_length(hexs_mobility(hex_id = "ID9", units_data = units_game, dist_matrix_data = dist_matrix), 126)
+  expect_length(hexs_mobility(hex_id = "ID344",mobility = 2, units_data = units_game, dist_matrix_data = dist_matrix), 10)
   expect_length(hexs_mobility(hex_id = "ID9",mobility = 0, units_data = units_game, dist_matrix_data = dist_matrix), 0)
-  expect_equal(hexs_mobility(hex_id = "ID352", units_data = units_game, dist_matrix_data = dist_matrix)[1], c("ID210" = 8))
-  expect_equal(hexs_mobility(hex_id = "ID352",mobility = 2, units_data = units_game, dist_matrix_data = dist_matrix)[1], c("ID320" = 2))
+  expect_equal(hexs_mobility(hex_id = "ID344", units_data = units_game, dist_matrix_data = dist_matrix)[1], c("ID282" = 3))
+  expect_equal(hexs_mobility(hex_id = "ID344",mobility = 2, units_data = units_game, dist_matrix_data = dist_matrix)[1], c("ID312" = 2))
 })
 
 test_that("check errors", {
-  expect_error(hexs_mobility(hex_id = "ID10", units_data = units_game, dist_matrix_data = dist_matrix))
+  expect_error(hexs_mobility(hex_id = "ID8", units_data = units_game, dist_matrix_data = dist_matrix))
   expect_error(hexs_mobility(hex_id = "ID9", units_data = units, dist_matrix_data = dist_matrix))
   expect_error(hexs_mobility(hex_id = "ID9", mobility = -10, units_data = units, dist_matrix_data = dist_matrix))
   expect_error(hexs_mobility(hex_id = "ID9", mobility = NA, units_data = units, dist_matrix_data = dist_matrix))
